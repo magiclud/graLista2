56c27a80307838192e9032fbc65a2f79d8bd66e5
Aga <agnieszkalud@op.pl>
2013-10-20 18:49:55 +0200

    testy dla układow kart w Player

--- src/Player.java
+++ src/Player.java
@@ -59,7 +59,108 @@ public class Player {
 		return false;
 	}
 
+	public boolean checkIfFourOfAKing() {
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getCourt().ordinal() == drugaKarta.getCourt().ordinal()
+				&& drugaKarta.getCourt().ordinal() == trzeciaKarta.getCourt().ordinal()
+				&& trzeciaKarta.getCourt().ordinal() == czwartaKarta.getCourt().ordinal()) {
+			return true;
+		}
+		if (drugaKarta.getCourt().ordinal() == trzeciaKarta.getCourt().ordinal()
+				&& trzeciaKarta.getCourt().ordinal() == czwartaKarta.getCourt().ordinal()
+				&& czwartaKarta.getCourt().ordinal() == piataKarta.getCourt().ordinal()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfFullHouse() {
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getCourt().ordinal() == drugaKarta.getCourt().ordinal()
+				&& drugaKarta.getCourt().ordinal() == trzeciaKarta.getCourt().ordinal()
+				&& czwartaKarta.getCourt().ordinal() == piataKarta.getCourt().ordinal()) {
+			return true;
+		}
+		if (pierwszaKarta.getCourt().ordinal() == drugaKarta.getCourt().ordinal()
+				&& trzeciaKarta.getCourt().ordinal() == czwartaKarta.getCourt().ordinal()
+				&& czwartaKarta.getCourt().ordinal() == piataKarta.getCourt().ordinal()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfFlush() {
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getSuit().ordinal() == drugaKarta.getSuit().ordinal()
+				&& drugaKarta.getSuit().ordinal() == trzeciaKarta.getSuit().ordinal()
+				&& trzeciaKarta.getSuit().ordinal() == czwartaKarta.getSuit().ordinal()
+				&& czwartaKarta.getSuit().ordinal() == piataKarta.getSuit().ordinal()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfStrit() {
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getCourt().ordinal() + 1 == drugaKarta.getCourt().ordinal()
+				&& pierwszaKarta.getCourt().ordinal() + 2 == trzeciaKarta.getCourt().ordinal()
+				&& pierwszaKarta.getCourt().ordinal() + 3 == czwartaKarta.getCourt().ordinal()
+				&& pierwszaKarta.getCourt().ordinal() + 4 == piataKarta.getCourt().ordinal()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfThreeOfAKind() {
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getCourt().equals(drugaKarta.getCourt()) && drugaKarta.getCourt().equals(trzeciaKarta.getCourt())) {
+			return true;
+		}
+		if (drugaKarta.getCourt().equals(trzeciaKarta.getCourt()) && trzeciaKarta.getCourt().equals(czwartaKarta.getCourt())) {
+			return true;
+		}
+		if (trzeciaKarta.getCourt().equals(czwartaKarta.getCourt()) && czwartaKarta.getCourt().equals(piataKarta.getCourt())) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfTwoPair(){
+		Card pierwszaKarta = ownCards.get(0);
+		Card drugaKarta = ownCards.get(1);
+		Card trzeciaKarta = ownCards.get(2);
+		Card czwartaKarta = ownCards.get(3);
+		Card piataKarta = ownCards.get(4);
+		if (pierwszaKarta.getCourt().equals(drugaKarta.getCourt()) || drugaKarta.getCourt().equals(trzeciaKarta.getCourt())
+				|| trzeciaKarta.getCourt().equals(czwartaKarta.getCourt()) || czwartaKarta.getCourt().equals(piataKarta.getCourt())) {
+			return true;
+		}
+		return false;
+	}
+
+
 	public void chooseCardsToExchange() {
+
 
 	}
 }
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -77,7 +77,90 @@ public class PlayerTest {
 		Player checkStraightFlush = new Player(someCards);
 
 		assertTrue(checkStraightFlush.checkIfStraightFlash());
+	}
 
+	@Test
+	public void checkIfPlayerHaveFourOfAKing() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.SPADE));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.CLUB));
+
+		Player checkFourOfAKing = new Player(someCards);
+
+		assertTrue(checkFourOfAKing.checkIfFourOfAKing());
+	}
+
+	@Test
+	public void checkIfPlayerHaveFullHouse() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.SPADE));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.CLUB));
+
+		Player checkFullHouse = new Player(someCards);
+
+		assertTrue(checkFullHouse.checkIfFullHouse());
+	}
+
+	@Test
+	public void checkIfPlayerHaveFlush() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.SEVEN, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.ACE, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.TEN, SuitEnum.CLUB));
+
+		Player checkFlush = new Player(someCards);
+
+		assertTrue(checkFlush.checkIfFlush());
+	}
+
+	@Test
+	public void checkIfPlayerHaveStrit() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.THREE, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.FOUR, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.SPADE));
+
+		Player checkStrit = new Player(someCards);
+
+		assertTrue(checkStrit.checkIfStrit());
+	}
+
+	@Test
+	public void checkIfPlayerHaveThreeOfAKind() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.SPADE));
+
+		Player checkThreeOfAKind = new Player(someCards);
+
+		assertTrue(checkThreeOfAKind.checkIfThreeOfAKind());
+	}
+
+	@Test
+	public void checkIfPlayerHaveTwoPair() {
+		someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.FIVE, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.QUEEN, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.SPADE));
+
+		Player checkTwoPair = new Player(someCards);
+
+		assertTrue(checkTwoPair.checkIfTwoPair());
 	}
 
 
