5264689324d82d88ca3201654ee097cd553b7f31
Aga <agnieszkalud@op.pl>
2013-10-20 10:45:00 +0200

    proba stworzenia metody sortujacej

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -34,4 +34,27 @@ public class DeckTest {
 		assertEquals(beforeToDeal, afterToDeal + 1);
 	}
 
+	@Test
+	public void testSpawdzaCzyKartySaPosortowane() {
+		ArrayList<Card> someCards = new ArrayList<>();
+		someCards.add(new Card(CourtEnum.FOUR, SuitEnum.HEART));
+		someCards.add(new Card(CourtEnum.KING, SuitEnum.SPADE));
+		someCards.add(new Card(CourtEnum.ACE, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		someCards.add(new Card(CourtEnum.TWO, SuitEnum.CLUB));
+		someCards.add(new Card(CourtEnum.SIX, SuitEnum.SPADE));
+
+		Deck checkSort = new Deck();
+		checkSort.sort(someCards);
+		ArrayList<Card> cardsInOrder = new ArrayList<>();
+		cardsInOrder.add(new Card(CourtEnum.ACE, SuitEnum.DIAMOND));
+		cardsInOrder.add(new Card(CourtEnum.KING, SuitEnum.SPADE));
+		cardsInOrder.add(new Card(CourtEnum.SIX, SuitEnum.SPADE));
+		cardsInOrder.add(new Card(CourtEnum.FOUR, SuitEnum.HEART));
+		cardsInOrder.add(new Card(CourtEnum.TWO, SuitEnum.DIAMOND));
+		cardsInOrder.add(new Card(CourtEnum.TWO, SuitEnum.CLUB));
+
+		assertEquals(someCards, cardsInOrder);
+
+	}
 }
