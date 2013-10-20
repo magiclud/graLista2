3f4f0a8f34a5e18821e46874c2cccb7d3f38b87c
Aga <agnieszkalud@op.pl>
2013-10-20 11:11:12 +0200

    pullowanie

--- src/Player.java
+++ src/Player.java
@@ -6,7 +6,7 @@ public class Player {
 
 
 	public Player(ArrayList<Card> givenCards) { // Player ma przecież dostawać karty od stołu !
-		if (givenCards.size() > 5) {
+		if (givenCards.size() > 5 || givenCards.size() < 5) {
 			throw new IllegalStateException("Niepoprawna ilosc kart dla gracza");
 		}
 		this.ownCards = givenCards;
