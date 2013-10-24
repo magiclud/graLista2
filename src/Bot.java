b7dd1d5e0488b7ed392822ee8a1429d4b81cc127
Aga <agnieszkalud@op.pl>
2013-10-24 14:31:12 +0200

    bot - strategia

--- src/Bot.java
+++ src/Bot.java
@@ -1,15 +1,42 @@
 import java.util.List;
-
+import java.util.Random;
 
 public class Bot extends Player {
+	Random botCards = new Random();
+	int howMany;
+	int choosenCart;
+	List<Integer> abandonedIndexes;
 
 	public Bot(List<Card> givenCards, Table currentTable) {
 		super(givenCards, currentTable);
 	}
 
 	List<Card> joinGame() {
-		// TODO Auto-generated method stub
-		return null;
+		Boolean isDone = false;
+		do {
+			isDone = selectCards();
+		} while (!isDone);
+
+		showCards();
+		return ownCards;
+	}
+
+	Boolean selectCards() {
+
+		howMany = botCards.nextInt(5);
+		for (int i = 0; i <= howMany; i++) {
+			// losuje imdeksy kart do usuniecia
+			abandonedIndexes.add(botCards.nextInt(5));
+		}
+		requestCards(abandonedIndexes);
+		return true;
+	}
+
+	// ta sama metoda w humanie
+	void showCards() {
+		for (int i = 0; i < ownCards.size(); ++i) {
+			System.out.print("[" + i + "]" + " " + ownCards.get(i) + " ");
+		}
 	}
 
 }
--- src/Human.java
+++ src/Human.java
@@ -1,68 +1,66 @@
-import java.io.DataInputStream;
-import java.util.ArrayList;
 import java.util.List;
 
-
 public class Human extends Player {
-
 
 	public Human(List<Card> someCards, Table currentTable) {
 		super(someCards, currentTable);
 	}
 
-	void showCards() { 
-		for(int i = 0; i < ownCards.size(); ++i) { 
-			System.out.print("[" + i + "]" + " " + ownCards.get(i) + " "); 
-		} 
+	void showCards() {
+		for (int i = 0; i < ownCards.size(); ++i) {
+			System.out.print("[" + i + "]" + " " + ownCards.get(i) + " ");
+		}
 	}
+
 	void askCards() {
 		System.out.println("Niech użytkownik wpisze jakie chce karty");
 	}
+
 	void askEnd() {
 		System.out.println("Niech użytkownik czy chce wchodzić w grę <<1>> czy chce pasować <<0>>");
 	}
+
 	// Pobiera karty indeksowane numerami 0,1,2,3,4
 	// żąda od użytkownika podania indeksów kart w formie np.
 	// Chcę wymienić 0,2,4 to wpisuję 0,2,4
-	Boolean selectCards(List<Integer> abandonedIndexes) { // Zwraca true jak się uda wymienić karty, false jak użytkownik coś pokręcił :)
-		if(abandonedIndexes == null) 
+	Boolean selectCards(List<Integer> abandonedIndexes) { // Zwraca true jak się
+															// uda wymienić
+															// karty, false jak
+															// użytkownik coś
+															// pokręcił :)
+		if (abandonedIndexes == null)
 			return true;
-		if(abandonedIndexes.size() > 4 )
+		if (abandonedIndexes.size() > 4)
 			return false;
-		for(int i = 0; i < abandonedIndexes.size(); ++i) {
-			if(abandonedIndexes.get(i) < 0 || abandonedIndexes.get(i) > 4  )
+		for (int i = 0; i < abandonedIndexes.size(); ++i) {
+			if (abandonedIndexes.get(i) < 0 || abandonedIndexes.get(i) > 4)
 				return false;
 		}
 		requestCards(abandonedIndexes);
 		return true;
 	}
-	
+
 	List<Card> joinGame() {
 		showCards();
-		
+
 		Boolean isDone = false;
 		do {
-		askCards();
-		List<Integer> abandonedIndexes = null;
-		// Tutaj będziemy pobierać indeksy od użytkownika
-		isDone = selectCards(abandonedIndexes);
-		}
-		while(!isDone);
-		
+			askCards();
+			List<Integer> abandonedIndexes = null;
+			// Tutaj będziemy pobierać indeksy od użytkownika
+			isDone = selectCards(abandonedIndexes);
+		} while (!isDone);
+
 		showCards();
-		
+
 		Boolean wannaPlay = false;
-		
+
 		askEnd();
-		
-		if(wannaPlay)
+
+		if (wannaPlay)
 			return ownCards;
-		 
+
 		return null;
 	}
-
-
-
-
 
 }
