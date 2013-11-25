8e5d21f7d042775cd3a982d8399ac1f48b2a35cd
Andrzej L <andrzejlud@op.pl>
2013-11-25 06:30:40 +0100

    Usuniecie nipotrzebnych metod, toString - to masz za darmo od enuma, a tamte dwie to był jakiś copy paste błedny.

--- src/StatusEnum.java
+++ src/StatusEnum.java
@@ -1,38 +1,3 @@
-
 public enum StatusEnum {
 	CLEAN, CHECK, BET, RAISE, CALL, ALL_IN, FOLD;
-
-	public String toString() {
-
-		switch (this) {
-		case CLEAN:
-			return "CLEAN";
-		case CHECK:
-			return "CHECK";
-		case BET:
-			return "BET";
-		case RAISE:
-			return "RAISE";
-		case CALL:
-			return "CALL";
-		case ALL_IN:
-			return "ALL-IN";
-		case FOLD:
-			return "FOLD";
-		}
-		throw new IllegalStateException("nieprawidlowy ruch");
-	}
-
-	public boolean isGreater(CourtEnum secondCourt) {
-		if (this.ordinal() > secondCourt.ordinal()) {
-			return true;
-		}
-		return false;
-	}
-
-	public boolean isLess(CourtEnum court) {
-		return this.ordinal() < court.ordinal();
-	}
-	
-
 }
