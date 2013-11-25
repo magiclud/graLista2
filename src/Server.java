abb84457c0218e3a57a79a75be6c854eff379c44
Andrzej L <andrzejlud@op.pl>
2013-11-25 06:58:32 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -3,96 +3,77 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+ 
 public class Server {
-	private static int port = 1793;
-	private ServerSocket server = null;
-
-	public Server() {
-		try {
-			server = new ServerSocket(port);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
-
-	public void listenSocket() {
-		System.out.println("Czekanie na polczenie od klienta...");
-		while (true) {// główna pętla serwera
-			try {
-				// stworz gniazdo
-				new ConnectionHandler(server.accept());
-			} catch (IOException e) {
-				System.out.println("Accept failed: 4444");
-				System.exit(-1);
-			}
-		}
-	}
-
-	class ConnectionHandler implements Runnable {
-		private Socket socket;
-
-		public ConnectionHandler(Socket socket) {
-			this.socket = socket;
-
-			Thread t = new Thread(this);
-			t.start();
-		}
-
-		public void run() {
-
-			System.out.println("nazwiazano polaczenie z klientem " + socket.getInetAddress());
-			System.out.println("Czekanie na graczy... ");
-
-			try {
-
-				ObjectInputStream strumienWejsciowy = new ObjectInputStream(socket.getInputStream());
-				ObjectOutputStream strumienWyjsciowy = new ObjectOutputStream(socket.getOutputStream());
-				System.out
-						.println("System:  Jeżeli chcesz zacząć grę, wpisz START i kliknij Wyslij wiadomosc.\n Jeżeli chcesz zakonczyc gre wpisz KONIEC.");
-
-				String message = (String) strumienWejsciowy.readObject();
-				strumienWyjsciowy.writeObject(" System: Wpisz ilu ma grać ludzi << 2<=numHum+numBot<=4 >>:");
-				String liczbaHumanow = (String) strumienWejsciowy.readObject();
-				strumienWyjsciowy.writeObject(" Liczba \"humanow\": " + liczbaHumanow
-						+ "\n System: Wpisz ile ma grać botów << 2<=numHum+numBot<=4 >>:");
-				String liczbaBotow = (String) strumienWejsciowy.readObject();
-				strumienWyjsciowy.writeObject(" Liczba botow: " + liczbaBotow + "\n");
-
-				while (message != "KONIEC") {
-					String[] tablica = message.split(" ");
-					System.out.println("Odczytano: " + message);
-					strumienWyjsciowy.writeObject("Wynik. Wygral: " + tablica[0]);
-					message = (String) strumienWejsciowy.readObject();
-				}
-
-				System.out.println("Klient konczy polaczenie z adresu: " + socket.getInetAddress());
-				strumienWyjsciowy.close();
-				strumienWejsciowy.close();
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-
-		Server example = new Server();
-		example.listenSocket();
-		example.stopSerwer();
-
-	}
-
-	private void stopSerwer() {
-		try {
-			server.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private ServerSocket server;
+    private int port = 7777;
+ 
+    public Server() {
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void main(String[] args) {
+        Server example = new Server();
+        example.handleConnection();
+    }
+ 
+    public void handleConnection() {
+        System.out.println("Waiting for client message...");
+ 
+        //
+        // The server do a loop here to accept all connection initiated by the
+        // client application.
+        //
+        while (true) {
+            try {
+                Socket socket = server.accept();
+                new ConnectionHandler(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+ 
+class ConnectionHandler implements Runnable {
+    private Socket socket;
+ 
+    public ConnectionHandler(Socket socket) {
+        this.socket = socket;
+ 
+        Thread t = new Thread(this);
+        t.start();
+    }
+ 
+    public void run() {
+        try
+        {
+            //
+            // Read a message sent by client application
+            //
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            String message = (String) ois.readObject();
+            System.out.println("Message Received: " + message);
+ 
+            //
+            // Send a response information to the client application
+            //
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject("Hi..." + message);
+ 
+            ois.close();
+            oos.close();
+            socket.close();
+ 
+            System.out.println("Waiting for client message...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
