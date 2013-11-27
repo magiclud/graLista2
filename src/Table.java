0384e2031727f1cb09f640d7e4433925a39bbac3
Andrzej L <andrzejlud@op.pl>
2013-11-27 07:21:53 +0100

    Usuniecie niepotrzebnej metody

--- src/Table.java
+++ src/Table.java
@@ -198,52 +198,6 @@ public class Table {
 		return actualDeck.giveCards(numbOfCards);
 	}
 
-	public List<Card> newSafeChangeCards(List<Integer> abandonedIndexes, String PlayerID) {
-		int numberCardsToReturn = abandonedIndexes.size();
-		Player player = findPlayer(PlayerID);
-		if (player.isNewAlreadyChangedCards())
-			throw new IllegalStateException("Już wymieniałeś karty !");
-		else
-			player.setNewAlreadyChangedCards(true);
-		if (numberCardsToReturn > 4) {
-			throw new IllegalStateException("Niepoprawna ilosc kart dla gracza");
-		}
-		List<Card> tempPlayerCards = player.ownCards;
-		Sorter s = new Sorter();
-		s.sort(tempPlayerCards);
-
-		/** usuwam karty po indeksie **/
-		// SORTOWANIE AHAHAHAH :D
-		int[] sorted = new int[abandonedIndexes.size()];
-		for (int i = 0; i < abandonedIndexes.size(); ++i) {
-			sorted[i] = abandonedIndexes.get(i);
-		}
-		Arrays.sort(sorted);
-		for (int i = 0; i < abandonedIndexes.size(); ++i) {
-			abandonedIndexes.set(i, sorted[abandonedIndexes.size() - 1 - i]);
-		}
-		// KONIEC SORTOWANIA
-		for (int i = 0; i < numberCardsToReturn; ++i) {
-			System.out.println("System: Gracz wymienia : [" + abandonedIndexes.get(i) + "]");
-			tempPlayerCards.remove((int) abandonedIndexes.get(i));
-		}
-		/*
-		 * for(int i = 0; i < tempPlayerCards.size(); ++i) {
-		 * System.out.println("Po usunieciu: " +
-		 * tempPlayerCards.get(i).getCourt() + "" +
-		 * tempPlayerCards.get(i).getSuit() + " " + i); }
-		 */
-		// Pobiera nowe karty
-		List<Card> tempCards = giveCards(abandonedIndexes.size());
-		for (int i = 0; i < tempCards.size(); ++i) {
-			tempPlayerCards.add(tempCards.get(i));
-			System.out
-					.println("System: Oddaję graczowi: " + tempCards.get(i).getCourt() + "" + tempCards.get(i).getSuit() + " [" + i + "]");
-		}
-		player.ownCards = tempPlayerCards;
-		return tempPlayerCards;
-	}
-
 	public void setCoinsIfOnePlayerWin() {
 		if (winners.size() == 1) {
 			// przekazanie wygranych pieniedzy graczowi
